3ba44ac9e1d19168bf72d61bf486fa5f28966420
hatisenangdotcom <hatisenangdotcom@gmail.com>
2024-11-18 07:41:26 +0000

    ddc04fa11a53

---
+++ wp-content/uploads/hs-tinymce-buttons/hs_tinymce_button_footnote.js
@@ -0,0 +1,14 @@
+(function() {
+	tinymce.PluginManager.add('hs_mce_button_footnote', function( editor, url ) {
+		editor.addButton('hs_mce_button_footnote', {
+			//text: 'Footnote',
+			//icon: false,
+			title : 'Footnote',
+			image : '/wp-content/uploads/hs-tinymce-buttons/footnote.png',
+			onclick: function() {
+				//editor.insertContent('WPExplorer.com is awesome!');
+				editor.selection.setContent('[efn_note]' + editor.selection.getContent() + '[/efn_note]');
+			}
+		});
+	});
+})();
